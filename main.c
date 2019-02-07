de9e821f58d59104425f461ed9795fb5538f0b0d
awhalverson15 <awhalverson15@users.noreply.github.com>
2019-02-07 14:01:21 -0500

    Update main.c
    random update

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "gcode_decoder.h"
 int main()
 {
-
+//test 123
     Location * loc = location_new(26,13,14);///dynamically allocates and constructs a location struct and returns a pointer to that struct
     printf("X:%d,Y:%d,Z:%d\n",loc->x_loc,loc->y_loc,loc->z_loc);
     char gcode1[75];
