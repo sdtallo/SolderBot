02b4a53feac8445a662bc4212921339f7664d821
tjriddle15 <47423627+tjriddle15@users.noreply.github.com>
2019-02-19 10:28:19 -0500

    Update caller.py

--- caller.py
+++ caller.py
@@ -3,9 +3,10 @@
 import serial
 import time
 
+# Open the serial port
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 
-# Set up controller  
+# Set up the controller  
 ser.write("\r\n\r\n")
 time.sleep(2)
 ser.flushInput()
@@ -13,28 +14,40 @@ ser.write("G20\n") # Set units to inches
 grbl_out = ser.readline()
 print("Set units to inches: " + grbl_out.strip())
 time.sleep(1)
-ser.write("G10 P0 L20 X0 Y0 Z0\n")
+ser.write("G10 P0 L20 X0 Y0 Z0\n") # Set home position
 grbl_out = ser.readline()
 print("Set home position: " + grbl_out.strip())
 time.sleep(1)
-#ser.write("G90\n") # Set absolute movement
-#grbl_out = ser.readline()
-#print("Set absolute movement: " + grbl_out.strip())
-#time.sleep(1)
-#ser.write("G1 F6") # Set feed rate to 6 in/min
-#time.sleep(1)
 ser.write("G0 Z3\n") # Move z up at max feed rate
 grbl_out = ser.readline()
 print("Move Z axis up: " + grbl_out.strip())
 time.sleep(1)
 
-# Read inputs from file
-with open("gfile.txt", r) as f
-  ser.write(f.readlines())
-  grbl_out = ser.readline()
-  time.sleep(3)
+# Declare variable for checking command type
+move = 0
 
-# Return home and end
+# Read inputs from the file
+with open("gfile.txt", r) as f
+  line = f.readline()
+  if "G0" in line:
+    move = 1
+  while line:
+    ser.write(line)
+    grbl_out = ser.readline()
+    time.sleep(3)
+    if move:
+      ser.write("G0 Z0\n") # Move z down
+      grbl_out = ser.readline()
+      time.sleep(1)
+      ser.write("G0 Z3\n") # Move z up
+      grbl_out = ser.readline()
+      time.sleep(1)
+      move = 0
+    line = f.readline()
+    if "G0" in line:
+      move = 1
+
+# Return home and close the serial port
 ser.write("G0 X0 Y0 Z0\n")
 grbl_out = ser.readline()
 time.sleep(5)
