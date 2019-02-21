6a28e9aee1a1caf918c7a140f921d325a74ec588
Sam Stensland <srstensland15@my.trine.edu>
2019-02-21 12:45:47 -0500

    Made quick correction to Form9.cs file

---
+++ Form9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Home
+{
+    public partial class Form9 : Form
+    {
+        public Form9(string leftPins, string middlePins, string rightPins)
+        {
+            InitializeComponent();
+            label6.Text = leftPins;
+            label7.Text = middlePins;
+            label8.Text = rightPins;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form5 f5 = new Form5();
+            f5.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.ShowDialog();
+        }
+    }
+}
