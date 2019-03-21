f43c7afe5eb732a1e4615b6cc848c129f3d31e47
awhalverson15 <awhalverson15@users.noreply.github.com>
2019-03-21 14:38:02 -0400

    Revert "fix merge bug"
    This reverts commit 049e655153c8a364ed5e5bef508d4e8a988bd032.

---
+++ GUI_Home/GUI_Home/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Home
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.ShowDialog();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form10 f10 = new Form10();
+            f10.ShowDialog();
+        }
+    }
+}
