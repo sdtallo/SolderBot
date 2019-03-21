85d301dbf2fcefd767a3ee1efc34276a7c302593
Sam Stensland <srstensland15@my.trine.edu>
2019-03-21 15:00:58 -0400

    Fixed file path for testMe.txt
    Going to test connecting to Andrew's .exe using a .txt first

--- GUI_Home/GUI_Home/Form9.cs
+++ GUI_Home/GUI_Home/Form9.cs
@@ -21,31 +21,7 @@ namespace GUI_Home
             label8.Text = rightPins;
 
             // Call Gcode program here
-            // Modified example from https://stackoverflow.com/questions/9679375/run-an-exe-from-c-sharp-code
-            const string ex1 = "C:\\";
-            const string ex2 = "C:\\Dir";
-
-            // Use ProcessStartInfo class
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = false;
-            startInfo.UseShellExecute = false;
-            startInfo.FileName = "seniorDesignP.exe";  //Gcode generator .exe
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-//            startInfo.Arguments = "-f j -o \"" + ex1 + "\" -z 1.0 -s y " + ex2;
-
-            try
-            {
-                // Start the process with the info we specified.
-                // Call WaitForExit and then the using statement will close.
-                using (Process exeProcess = Process.Start(startInfo))
-                {
-                    exeProcess.WaitForExit();
-                }
-            }
-            catch
-            {
-                // Log error.
-            }
+            Process.Start("testMe.txt");
         }
 
         private void button1_Click(object sender, EventArgs e)
