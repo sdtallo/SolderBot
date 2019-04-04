867f31a88b7c51b43cceb9469a900e2764e6a56d
tjriddle15 <47423627+tjriddle15@users.noreply.github.com>
2019-04-04 12:00:34 -0400

    Add files via upload
    Uploaded up to date version

--- caller.py
+++ caller.py
@@ -1,96 +1,91 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 
 # This program reads gcode from a file and runs it through the sender
+# Current version of this makes test.py obsolete
 
 import serial
 import time
 import RPi.GPIO as GPIO
 
-z_down = "G0 Z0.45 \n" # Height of top of board
-solder_speed = 150 # PWM frequency
-extract_time = 0.5 # How long to extract solder
-solder_time = 5 # How long to hold iron down
+print("Program starting\n")
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
-p = GPIO.PWM(12, solder_speed)
+GPIO.setup(3, GPIO.OUT)
+GPIO.output(3, GPIO.HIGH)
+p = GPIO.PWM(12, 150) # Set PWM frequency
+p.start(0)
+time.sleep(0.01)
+p.stop()
 
 
 # Open the serial port
 ser = serial.Serial('/dev/ttyUSB0', 115200)
-time.sleep(0.1)
 
 # Set up the controller  
 ser.write("\r\n\r\n")
 time.sleep(2)
 ser.flushInput()
-# Set units to inches
-ser.write("G20\n")
+ser.write("G20\n") # Set units to inches
 grbl_out = ser.readline()
 print("Set units to inches: " + grbl_out.strip())
-# Set home position
-ser.write("G10 P0 L20 X0 Y0 Z0\n")
+time.sleep(1)
+ser.write("G10 P0 L20 X0 Y0 Z0\n") # Set home position
 grbl_out = ser.readline()
 print("Set home position: " + grbl_out.strip())
-# Move z up
-ser.write("G0 Z2\n")
+time.sleep(1)
+ser.write("G0 Z1\n") # Move z up at max feed rate
 grbl_out = ser.readline()
 print("Move Z axis up: " + grbl_out.strip())
-
-# Let serial buffer catch up
-time.sleep(10)
+time.sleep(1)
 
 # Declare variable for checking command type
 move = 0
 
 # Read inputs from the file
-with open("gfile.txt", "r") as f:
-  line = f.readline()
-  if "G0" in line:
-    move = 1
-  while line:
-    ser.write(line)
-    grbl_out = ser.readline()
-    ser.write("?\n") # Get status
-    grbl_out = ser.readline()
-    # Wait until command has finished
-    while "Idle" not in grbl_out:
-        time.sleep(0.2)
-        ser.write("?\n")
-        grbl_out = ser.readline()
-        
-    if move:
-      ser.write(z_down) # Move z down
-      grbl_out = ser.readline()
-      ser.write("?\n") # Get status
-      grbl_out = ser.readline()
-      # Wait until command has finished
-      while "Idle" not in grbl_out:
-        time.sleep(0.2)
-        ser.write("?\n")
-        grbl_out = ser.readline()
-      p.start(50)
-      time.sleep(extract_time)
-      p.stop()
-      time.sleep(solder_time)
-      ser.write("G0 Z2\n") # Move z up
-      grbl_out = ser.readline()
-      ser.write("?\n") # Get status
-      grbl_out = ser.readline()
-      # Wait until command has finished
-      while "Idle" not in grbl_out:
-        time.sleep(0.2)
-        ser.write("?\n")
-        grbl_out = ser.readline()
-      move = 0
+with open("gCodeLoc.txt", "r") as f:
     line = f.readline()
-    if "G0" in line:
-      move = 1
+    if "Z" in line:
+        solder = 1
+    else:
+        solder = 0
+    while line:
+        ser.write(line)
+        grbl_out = ser.readline()
+        print("Moving")
+        ser.write("?\n")
+        grbl_out = ser.readline()
+        while "Idle" not in grbl_out:
+            time.sleep(0.5)
+            ser.write("?\n")
+            grbl_out = ser.readline()
+        if solder:
+            print("Extracting")
+            p.start(50)
+            #temp = raw_input("Press enter to stop solder\n")
+            time.sleep(1)
+            p.stop()
+            print("Soldering")
+            #temp = raw_input("Press enter to continue\n")
+            time.sleep(1)
+            ser.write("G0 Z1\n") # Move z up
+            grbl_out = ser.readline()
+            print("Lifting")
+            #temp = raw_input("Press enter to continue\n")
+            #time.sleep(1)
+            solder = 0
+        #else:
+        #    time.sleep(5)
+        line = f.readline()
+        if "Z" in line:
+            solder = 1
 
 # Return home and close the serial port
 ser.write("G0 X0 Y0\n")
 grbl_out = ser.readline()
+time.sleep(1)
 ser.write("G0 Z0\n")
 grbl_out = ser.readline()
-time.sleep(15)
+time.sleep(5)
 ser.close()
